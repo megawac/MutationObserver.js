755749c68b4acec054054ec9869722ff2bc58419
Graeme Yeates <yeatesgraeme@gmail.com>
2014-01-01 17:51:59 -0500

    Added performance tests

---
+++ test/perf.js
@@ -0,0 +1,72 @@
+define([], function() {
+    return function(Custom, Native) {
+        var small = {
+            $ele: $("<div class='test'></div>"),
+            children: 10,
+            descendents: 1
+        };
+        var mid = {
+            $ele: $("<div class='test'></div>"),
+            children: 20,
+            descendents: 3
+        };
+        var large = {
+            $ele: $("<div class='test'></div>"),
+            children: 50,
+            descendents: 5
+        };
+
+        function fill(opts) {
+            var str = "";
+            for (var i = 0, j; i < opts.children; i++) {
+                for (j = 0; j < opts.descendents; j++) {
+                    str += "<span>" + j + "</span>";
+                }
+                str+=i;
+            }
+            opts.$ele.html(str);
+        };
+
+        fill(small);
+        fill(mid);
+        fill(large);
+
+        Custom.prototype.options.period = Infinity;//dont check
+
+        [{
+            childList: true
+        },
+        {
+            childList: true,
+            subtree: true
+        },
+        {
+            attributes: true
+        },
+        {
+            attributes: true,
+            attributeFilter: ["id", "style"]
+        }].forEach(function(context) {
+
+            [small, mid, large].forEach(function(test) {
+                var ele = test.$ele[0];
+
+                var description = Object.keys(context).join("+") + " search on element with " + test.children + " children and " + test.descendents + " descendents";
+
+                var observer = new Custom(function(items, observer) {});
+                observer.observe(test.$ele.get(0), context);
+                JSLitmus.test("Shimmed MutationObserver: " + description, function() {
+                    observer.takeRecords();
+                });
+
+                if(Native) {
+                    var observer2 = new Native(function(items, observer) {});
+                    observer2.observe(test.$ele.get(0), context);
+                    JSLitmus.test("Native MutationObserver: " + description, function() {
+                        observer2.takeRecords();
+                    });
+                }
+            });
+        });
+    }
+});
--- test/testmanager.js
+++ test/testmanager.js
@@ -3,18 +3,26 @@ QUnit.config.autostart = false;//was having trouble with autostart
 
 //Simple test manager to delete window.MutationObserver after the original test runs
 $(function() {
+    var isPhantom = /phantomjs/i.test(navigator.userAgent);
 
-    curl(["tests.js"], function(MutationObserverTests) {
+    curl(["tests.js", "perf.js"], function(MutationObserverTests, perf) {
 
         QUnit.start();
 
         var test2 = function(context) {
             if(context && context.name !== "MutationObserver") return;
 
+            var native = window.WebkitMutationObserver || window.MutationObserver;
+
             window.WebkitMutationObserver = window.MutationObserver = null;//so poly goes in
 
             yepnope.injectJs("../MutationObserver.js", function() {
                 MutationObserverTests("MutationObserver-Shim");
+
+                var custom = window.MutationObserver;
+
+                if(native != custom)//chrome bug
+                    perf(custom, native);
             });
         };
 
@@ -24,6 +32,5 @@ $(function() {
         } else {
             test2();
         }
-        
     });
 });
