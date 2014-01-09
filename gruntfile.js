6d0142e5ffc99740a2b31530f81edd2e4b6aa5cd
megawac <megawac@gmail.com>
2014-01-09 18:25:53 -0500

    Fix silly syntax error...

--- gruntfile.js
+++ gruntfile.js
@@ -66,6 +66,7 @@ module.exports = function(grunt) {
                             browserName: "safari",
                             version: "5"
                         }
+                    ]
                 }
             }
         },
