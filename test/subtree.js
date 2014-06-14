77b08b9f16d47902d4d72cfd9ffb301bb5334c45
megawac <yeatesgraeme@gmail.com>
2014-06-14 17:02:42 -0400

    Test subtree observing element with comments node

--- test/subtree.js
+++ test/subtree.js
@@ -1,7 +1,7 @@
 define(["utils"], function(utils) {
     return function() {//tests        
 
-        QUnit.asyncTest("subtree", 12, function() {
+        QUnit.asyncTest("subtree", 13, function() {
             var deferred = utils.asyncAutocomplete(300);
 
             var $test = $("<div><span></span></div>");
@@ -71,22 +71,39 @@ define(["utils"], function(utils) {
             var $test2 = $("<div><span class='name'>3</span><span class='name'>2</span><span class='name'>1</span></div>");
             var teste2 = $test2.get(0);
             var $tar2 = utils.$randomChild($test2);
-            var observer3 = new MutationObserver(function(items, observer) {
+            var observer2 = new MutationObserver(function(items, observer) {
                 ok(items.every(function(item) {return item.type === "attributes"; }), "Can observe only attributes on the subtree");
 
                 equal(items.length, 1, "Can observe attributes of subtree");
                 equal(items[0].target, $tar2.get(0), "Called with the correct target on the subtree");
                 equal(items[0].attributeName, "class", "Called with the correct attribute name in the subtree");
             });
-            observer3.observe(teste2, {
+            observer2.observe(teste2, {
                 attributes: true,
                 subtree: true
             });
             $tar2.get(0).className += "attribute subtree test";
             $tar2.append("<span class='a'>text</span>");
 
+            // #8
+            var observer3 = new MutationObserver(function(items, observer) {
+
+            });
+            var $test3 = $("<div><span class='name'>3</span><span class='name'>2</span><span class='name'>1</span></div>");
+            $test3.prepend(document.createComment("don't break on comments"));
+            observer3.observe($test3.get(0), {
+                "attributes" : true,
+                "attributeFilter" : [ "height", "style" ],
+                "childList" : true,
+                "subtree" : true
+            });
+            ok(observer3.takeRecords(), [], "doesnt break on comments");
+
+
             deferred.done(function() {
                 observer.disconnect();
+                observer2.disconnect();
+                observer3.disconnect();
             });
         });
     };
